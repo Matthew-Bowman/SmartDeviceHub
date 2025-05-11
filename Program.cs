6583697db2bb2e6cbd57597a5e89b40f81304e55
Matthew <matthew.bowman@tutanota.com>
2025-05-11 14:57:56 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // Declare Variables
+        CancellationTokenSource cts = new CancellationTokenSource();
+        int udpPort = 5000;
+
+
+
+        // Initialize components
+        var udpListener = new UDPListener(udpPort);
+
+
+
+        // Subscribe Events
+        udpListener.PacketReceived += OnUDPMessage;
+        
+        
+        
+        // Start Systems
+        await udpListener.StartServer(cts.Token);
+    }
+
+    public static void OnUDPMessage(object? pSender, UdpReceiveResult pUDPResult) {
+        Console.WriteLine(Encoding.UTF8.GetString(pUDPResult.Buffer));
+    }
+}
---
+++ UDPListener/UDPListener.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+public class UDPListener
+{
+    // Member Variables
+    private int _port;
+    private UdpClient? _udpClient;
+
+    // Events
+    public event EventHandler<UdpReceiveResult>? PacketReceived;
+
+    // Constructors
+    public UDPListener(int pPort)
+    {
+        // Member Variable Assigning
+        this._port = pPort;
+    }
+
+    // Private Methods
+    protected virtual void OnPacketReceived(UdpReceiveResult pResult)
+    {
+        PacketReceived?.Invoke(this, pResult);  // Trigger the event
+    }
+
+    // Public Methods
+    public async Task StartServer(CancellationToken pCancellationToken)
+    {
+        // Start UDP Client
+        this._udpClient = new UdpClient(this._port);
+
+        // Listen For Packet
+        while (!pCancellationToken.IsCancellationRequested) {
+
+            try
+            {
+
+                UdpReceiveResult result = await this._udpClient.ReceiveAsync();
+                OnPacketReceived(result);
+
+            }
+            catch (Exception pException) {
+
+                Console.WriteLine($"[UDPListener] Error: {pException.Message}");
+
+            }
+
+        }
+
+        // Close UDP Client
+        this._udpClient.Close();
+    }
+}
